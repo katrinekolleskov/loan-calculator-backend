6c19108b07825b89fe1356bb42b14103744d79d3
katrinekolleskov <katrineloise@hotmail.com>
2021-06-08 15:21:37 +0200

    First commit

---
+++ src/main/java/com/example/loancalculatorbackend/LoanCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.loancalculatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loancalculatorbackend/LoanCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loancalculatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
