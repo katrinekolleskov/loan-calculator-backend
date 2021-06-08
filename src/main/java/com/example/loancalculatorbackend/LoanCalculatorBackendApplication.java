299b341d1912265b8bf06a13f4ce8e774fa011cb
katrinekolleskov <katrineloise@hotmail.com>
2021-06-08 15:36:50 +0200

    first test

--- src/main/java/com/example/loancalculatorbackend/LoanCalculatorBackendApplication.java
+++ src/main/java/com/example/loancalculatorbackend/LoanCalculatorBackendApplication.java
@@ -2,12 +2,20 @@ package com.example.loancalculatorbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LoanCalculatorBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanCalculatorBackendApplication.class, args);
 	}
 
+	@GetMapping
+	public String helloworld () {
+		return "hello world";
+	}
 }
+
